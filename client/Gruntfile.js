69f5bf83773d83b50ebb380fccdc4305a111091f
tae-jun <i2r.jun@gmail.com>
2014-10-18 19:21:19 +0900

    fix grunt build system

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,5 +1,5 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-concat');
@@ -7,9 +7,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-ts');
 
     // Default task.
-    grunt.registerTask('default', ['clean', 'concat', 'copy']);
+    grunt.registerTask('default', ['clean', 'ts', 'concat', 'copy']);
 
     // Project configuration.
     grunt.initConfig({
@@ -54,7 +55,7 @@ module.exports = function(grunt) {
         },
         copy: {
             tpl: {
-                files:[
+                files: [
                     {
                         expand: true,
                         cwd: 'src',
@@ -75,6 +76,36 @@ module.exports = function(grunt) {
                 files: ['<%= src.tsout %>', 'src/**/*.css', 'src/**/*.html'],
                 tasks: ['default']
             }
-        }
+        },
+        ts: {
+            // A specific target
+            build: {
+                // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
+                src: ["./src/**/*.ts"],
+                // The source html files, https://github.com/grunt-ts/grunt-ts#html-2-typescript-support
+                //html: ["test/work/**/*.tpl.html"],
+                // If specified, generate this file that to can use for reference management
+                //reference: "./test/reference.ts",
+                // If specified, generate an out.js file which is the merged js file
+                out: 'tsout/out.js',
+                // If specified, the generate JavaScript files are placed here. Only works if out is not specified
+                //outDir: 'test/outputdirectory',
+                // If specified, watches this directory for changes, and re-runs the current target
+                //watch: 'test',
+                // Use to override the default options, http://gruntjs.com/configuring-tasks#options
+                options: {
+                    // 'es3' (default) | 'es5'
+                    target: 'es5',
+                    // 'amd' (default) | 'commonjs'
+                    //module: 'commonjs',
+                    // true (default) | false
+                    sourceMap: false,
+                    // true | false (default)
+                    declaration: false,
+                    // true (default) | false
+                    removeComments: true
+                },
+            }
+        },
     });
 };
--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -1,66 +1,50 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-jasmine-node');
     grunt.loadNpmTasks('grunt-execute');
+    grunt.loadNpmTasks('grunt-ts');
 
     // Default task.
-    grunt.registerTask('default', ['execute']);
+    grunt.registerTask('default', ['ts', 'execute']);
 
     // Project configuration.
     grunt.initConfig({
+
+        // Task configuration.
         execute: {
-            target: {
+            server: {
                 src: ['app.js']
             }
         },
-        // Task configuration.
-        jasmine_node: {
-            options: {
-                forceExit: true,
-                match: '.',
-                matchall: false,
-                extensions: 'js',
-                specNameMatcher: 'spec'
-            },
-            all: ['test/']
-        },
-        jshint: {
-            options: {
-            curly: true,
-            eqeqeq: true,
-            immed: true,
-            latedef: true,
-            newcap: true,
-            noarg: true,
-            sub: true,
-            undef: true,
-            unused: true,
-            boss: true,
-            eqnull: true,
-            globals: {
-                jQuery: true
-            }
-            },
-            gruntfile: {
-            src: 'Gruntfile.js'
-            },
-            lib_test: {
-            src: ['lib/**/*.js', 'test/**/*.js']
-            }
-        },
-        nodeunit: {
-            files: ['test/**/*_test.js']
-        },
-        watch: {
-            gruntfile: {
-            files: '<%= jshint.gruntfile.src %>',
-            tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-            files: '<%= jshint.lib_test.src %>',
-            tasks: ['jshint:lib_test', 'nodeunit']
+        ts: {
+            // A specific target
+            build: {
+                // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
+                src: ["./**/*.ts", '!,.Scripts/**/*', '!./node_modules/**/*'],
+                // The source html files, https://github.com/grunt-ts/grunt-ts#html-2-typescript-support
+                //html: ["test/work/**/*.tpl.html"],
+                // If specified, generate this file that to can use for reference management
+                //reference: "./test/reference.ts",
+                // If specified, generate an out.js file which is the merged js file
+                //out: 'test/out.js',
+                // If specified, the generate JavaScript files are placed here. Only works if out is not specified
+                //outDir: 'test/outputdirectory',
+                // If specified, watches this directory for changes, and re-runs the current target
+                //watch: 'test',
+                // Use to override the default options, http://gruntjs.com/configuring-tasks#options
+                options: {
+                    // 'es3' (default) | 'es5'
+                    target: 'es5',
+                    // 'amd' (default) | 'commonjs'
+                    module: 'commonjs',
+                    // true (default) | false
+                    sourceMap: false,
+                    // true | false (default)
+                    declaration: false,
+                    // true (default) | false
+                    removeComments: true
+                },
             }
         }
     });
