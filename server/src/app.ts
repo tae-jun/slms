6c9034b240632f4bfd748d59a0fdd4f1daed4f20
tae-jun <i2r.jun@gmail.com>
2015-01-06 22:48:19 +0900

    Now building is available without VS

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
-﻿/// <reference path="../../server/scripts/typings/express/express.d.ts" />
-/// <reference path="../../server/scripts/typings/node/node.d.ts" />
+﻿/// <reference path="../../server/scripts/typings/node/node.d.ts" />
+/// <reference path="../scripts/typings/express-/express.d.ts" />
+/// <reference path="../scripts/typings/mongodb/mongodb.d.ts" />
 
 // node modules
 import http = require('http');
@@ -13,7 +14,7 @@ import config = require('./config');
 var app = express();
 
 // router configures
-require('./lib/routes/static/static')(app);
+require('./routes/static/static')(app);
 app.use(express.json());        // to support JSON-encoded bodies
 app.use(express.urlencoded());  // to support URL-encoded bodies
 
@@ -22,7 +23,7 @@ app.use(express.logger('dev'));
 app.use(app.router);
 
 // routes
-require('./lib/routes/index')(app);
+require('./routes/index')(app);
 
 http.createServer(app).listen(config.port, function () {
     console.log('Express server listening on port ' + config.port);
